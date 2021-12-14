4318d3466c413d17027ea74a643eba349d8e1624
tibizy <tobisuleodu@ymail.com>
2021-12-14 07:44:09 -0600

    first commit

---
+++ AlgoProblems/Numbers/FindAllPairsOfElementInArrayWithTargetSum.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class FindAllPairsOfElementInArrayWithTargetSum
+    {
+        public static void Main(string[] args)
+        {
+            System.print("Hello");
+        }
+
+        
+    }
